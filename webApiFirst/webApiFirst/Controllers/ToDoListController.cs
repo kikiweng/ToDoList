81405de535638781532726e5721bbd55dbdef656
chichiweng <emily091131027@gms.tku.edu.tw>
2018-12-30 21:26:39 +0800

    GET: http://localhost:52678/api/ToDoList return Hellow World

---
+++ webApiFirst/webApiFirst/Controllers/ToDoListController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace webApiFirst.Controllers
+{
+    [AllowAnonymous]
+    [Authorize]
+    public class ToDoListController : ApiController
+    {
+        // GET api/values/5
+        public string Get()
+        {
+            return "Hellow World";
+        }
+
+    }
+}
